4e9b7a3e62c7cd512021bb1addfcf12bc45aaab2
Mastervoliumpl <kubafijalkowski25@gmail.com>
2024-10-03 20:08:43 +0200

    Reversed Entity Framework

---
+++ RazorBlogProtfolio/Pages/Privileged/EditorPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorBlogProtfolio.Pages.Privileged
+{
+    public class EditorPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
